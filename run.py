491acb8fc204f6548c66c0c19544583e341bfd45
Luis Felipe Serna Mendoza <lfserna@umsa.bo>
2026-06-24 00:02:53 -0400

    Agregar entrada de ejecución Flask

---
+++ run.py
@@ -0,0 +1,10 @@
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(
+        host=app.config.get("APP_HOST", "0.0.0.0"),
+        port=app.config.get("APP_PORT", 5000),
+        debug=app.config.get("FLASK_ENV") == "development",
+    )
